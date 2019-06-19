cdf6fbc852be921012e924dac0a505006a5ece91
Kiearea <raleigh_f@yahoo.com>
2019-06-19 10:50:54 -0500

    Finished! Woo hoo! Go Rachel, it's ya birf-day!

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.*;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -7,7 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.swing.text.Position;
 import javax.validation.Valid;
 
 /**
@@ -21,10 +24,11 @@ public class JobController {
 
     // The detail display for a given Job at URLs like /job?id=17
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index(Model model, int id) {
+    public String index(Model model, @RequestParam(defaultValue = "0") int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
-
+        Job someJob = jobData.findById(id);
+        model.addAttribute("someJob", someJob);
         return "job-detail";
     }
 
@@ -41,7 +45,36 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
-        return "";
+        if (errors.hasErrors()) {
+
+            model.addAttribute("name", jobForm);
+            model.addAttribute("employer", jobForm);
+            model.addAttribute("location", jobForm);
+            model.addAttribute("coreCompetencies", jobForm);
+            model.addAttribute("positionType", jobForm);
+
+            return "new-job";
+        }
+
+        //gets all employers from jobData, Finds the ID associated with each employer and matches it with the employer ID from the jobForm
+        Employer emp = jobData.getEmployers().findById(jobForm.getEmployerId());
+        Location loc = jobData.getLocations().findById(jobForm.getLocationId());
+        PositionType pos = jobData.getPositionTypes().findById(jobForm.getPositionTypeId());
+        CoreCompetency core = jobData.getCoreCompetencies().findById(jobForm.getCoreCompetenciesId());
+
+        Job someJob = new Job(
+            jobForm.getName(),
+            emp,
+            loc,
+            pos,
+            core
+        );
+
+        model.addAttribute("someJob", someJob);
+
+        jobData.add(someJob);
+
+        return "job-detail";
 
     }
 }
